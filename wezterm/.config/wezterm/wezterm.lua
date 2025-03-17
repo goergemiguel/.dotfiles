d5c757e3e13d1d7d922940c6f599b6f6e3d3e8b9
Goerge <goergemiguel21@gmail.com>
2025-03-17 17:01:57 +0800

    change fontsize for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 
 config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
-config.font_size = 14
+config.font_size = 13
 
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_tab_bar = false
