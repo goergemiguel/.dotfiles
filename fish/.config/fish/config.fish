76150cb1216721d406795039a20b921becd65816
Goerge <goergemiguel21@gmail.com>
2025-03-17 10:37:29 +0800

    feat: add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# Commonly used git commands
+alias ga='git add'
+alias gc='git commit -m'
+alias gp='git push'
+alias gpf='git push --force'
+alias gs='git status'
+alias gl='git log'
